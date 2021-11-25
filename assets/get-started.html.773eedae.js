12a39778c5826d73522a7082d1e255277e3c8a7d
VanishMax <maxkors2014@gmail.com>
2021-11-25 08:51:21 +0000

    Deploying to gh-pages from @ code-pilots/magner@55c02022531efe28d776dd982b4b41a16c954e1b 🚀

--- assets/get-started.html.773eedae.js
+++
@@ -1,1 +0,0 @@
-import{c as e,o as t,a,b as r}from"./app.1e984e52.js";import{_ as s}from"./plugin-vue_export-helper.5a098b48.js";const o={},c={id:"get-started",tabindex:"-1"},_=a("a",{class:"header-anchor",href:"#get-started","aria-hidden":"true"},"#",-1),d=r(" Get started"),n=[_,d];function i(h,p){return t(),e("h1",c,n)}var m=s(o,[["render",i]]);export{m as default};
