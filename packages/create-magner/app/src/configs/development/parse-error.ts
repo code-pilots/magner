1e3aa60fa8cf5c345eb365c2751de165dd8e6018
VanishMax <maxkors2014@gmail.com>
2022-01-20 16:11:06 +0300

    feat(create-magner): update boilerplate application to match with new Request API

--- packages/create-magner/app/src/configs/development/parse-error.ts
+++
@@ -1,9 +0,0 @@
-import { errorParserController } from 'magner';
-
-/** Parse an error from the server request to field errors or a global error message */
-const errorParser = errorParserController((err) => ({
-  fields: err?.data?.errors?.fields || {},
-  message: err?.data?.message || '',
-}));
-
-export default errorParser;
