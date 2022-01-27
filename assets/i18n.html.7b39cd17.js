9f0b99ca9f8c82388b5f7c90ccf85f29dfe24d03
VanishMax <maxkors2014@gmail.com>
2022-01-27 13:20:05 +0000

    Deploying to gh-pages from @ code-pilots/magner@34db23cc8e2569734eb22e8885b73cf3cb04c01e 🚀

--- assets/i18n.html.7b39cd17.js
+++
@@ -1,1 +0,0 @@
-const t={key:"v-0ed12c89",path:"/configuration/i18n.html",title:"Translation",lang:"en",frontmatter:{},excerpt:"",headers:[{level:2,title:"Translation in custom views",slug:"translation-in-custom-views",children:[]}],filePathRelative:"configuration/i18n.md",git:{updatedTime:1643205294e3,contributors:[{name:"VanishMax",email:"maxkors2014@gmail.com",commits:1}]}};export{t as data};
