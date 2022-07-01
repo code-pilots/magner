15c6bb3d53b5a2e972aaf80b20fa7a6411a7c1c1
VanishMax <maxkors2014@gmail.com>
2022-07-01 11:05:28 +0000

    Deploying to gh-pages from @ code-pilots/magner@93c30b5b3fd0e4375916df02f0aac98a557df563 🚀

---
+++ assets/contributors.html.322798be.js
@@ -0,0 +1,1 @@
+const t={key:"v-20e7df08",path:"/contributors.html",title:"Contributors",lang:"en",frontmatter:{},excerpt:"",headers:[{level:2,title:"Project setup",slug:"project-setup",children:[]}],filePathRelative:"contributors.md",git:{updatedTime:1656673482e3,contributors:[{name:"VanishMax",email:"maxkors2014@gmail.com",commits:1}]}};export{t as data};
