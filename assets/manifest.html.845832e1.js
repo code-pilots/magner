2a1011283b3dd2228724e9252e54fe7b9b9cbf64
VanishMax <maxkors2014@gmail.com>
2022-07-21 13:49:13 +0000

    Deploying to gh-pages from @ code-pilots/magner@2c407e0aad6cd70f24ba3bee6f8a7cf27952b135 🚀

--- assets/manifest.html.845832e1.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('{"key":"v-e991b7d0","path":"/configuration/manifest.html","title":"Manifest controller","lang":"en","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1658410893000,"contributors":[{"name":"VanishMax","email":"maxkors2014@gmail.com","commits":1}]},"filePathRelative":"configuration/manifest.md"}');export{t as data};
