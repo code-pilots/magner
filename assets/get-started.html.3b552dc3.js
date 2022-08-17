3ef2fdbbb48c91be72134c96f36b9ba9eda62f1e
VanishMax <maxkors2014@gmail.com>
2022-08-17 11:11:27 +0000

    Deploying to gh-pages from @ code-pilots/magner@e4e833074000d1435552083e8561219fa4ea9a9a 🚀

---
+++ assets/get-started.html.3b552dc3.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-6c0089f0","path":"/get-started.html","title":"Get started","lang":"en","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1660734637000,"contributors":[{"name":"Max Korsunov","email":"maxkors2014@gmail.com","commits":1}]},"filePathRelative":"get-started.md"}');export{t as data};
---
+++ assets/manifest.html.8331d4fb.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-e991b7d0","path":"/configuration/manifest.html","title":"Manifest controller","lang":"en","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1660734637000,"contributors":[{"name":"Max Korsunov","email":"maxkors2014@gmail.com","commits":1}]},"filePathRelative":"configuration/manifest.md"}');export{t as data};
