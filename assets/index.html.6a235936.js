d3bf94743fd1085b9e14a977391e85d2a85921c8
VanishMax <maxkors2014@gmail.com>
2022-09-07 11:37:33 +0000

    Deploying to gh-pages from @ code-pilots/magner@d52d316b9e4dbc7ad29896b4ab555b10f9270186 🚀

---
+++ assets/index.html.6a235936.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-2bc0f532","path":"/configuration/","title":"Entry point","lang":"en","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1662550607000,"contributors":[{"name":"Max Korsunov","email":"maxkors2014@gmail.com","commits":1}]},"filePathRelative":"configuration/readme.md"}');export{t as data};
