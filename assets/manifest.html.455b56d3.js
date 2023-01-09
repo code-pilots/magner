eec454f5b5a4f3ec0f9fe1d180364a3e12687ae1
ddtikhonov <71875386+ddtikhonov@users.noreply.github.com>
2023-01-09 10:05:14 +0000

    Deploying to gh-pages from @ code-pilots/magner@650183b2a2288204103991c4c64968e06e392334 🚀

--- assets/manifest.html.455b56d3.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('{"key":"v-e991b7d0","path":"/configuration/manifest.html","title":"Manifest controller","lang":"en","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1673258608000,"contributors":[{"name":"ddtikhonov","email":"71875386+ddtikhonov@users.noreply.github.com","commits":1}]},"filePathRelative":"configuration/manifest.md"}');export{t as data};
