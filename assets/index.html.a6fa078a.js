119af2ca0a18542547f0b666e88254ec27bfd3bf
Fedor531 <60313644+Fedor531@users.noreply.github.com>
2023-06-20 12:37:34 +0000

    Deploying to gh-pages from @ code-pilots/magner@591cf6af951fa1f429c71990ece8324bac9f15d1 🚀

--- assets/index.html.a6fa078a.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('{"key":"v-2bc0f532","path":"/configuration/","title":"Entry point","lang":"en","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1687264518000,"contributors":[{"name":"ddtikhonov","email":"71875386+ddtikhonov@users.noreply.github.com","commits":1}]},"filePathRelative":"configuration/readme.md"}');export{t as data};
