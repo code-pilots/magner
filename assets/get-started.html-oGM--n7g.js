932d773f3137443c16f8f5e9915bec19e9ca2f60
Fedor531 <60313644+Fedor531@users.noreply.github.com>
2023-12-28 05:51:06 +0000

    Deploying to gh-pages from @ code-pilots/magner@4096d799651f8202bd11962f78294532707e5e92 🚀

--- assets/get-started.html-oGM--n7g.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-6c0089f0","path":"/get-started.html","title":"Get started","lang":"en","frontmatter":{},"headers":[],"git":{"updatedTime":1703742583000,"contributors":[{"name":"Fedor Pereverzev","email":"fedor531@yandex.ru","commits":1}]},"filePathRelative":"get-started.md"}');export{e as data};
