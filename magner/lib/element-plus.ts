88539ed2940759ab9fe984693c8ff1e5502f077c
Fedor Pereverzev <fedor531@yandex.ru>
2024-03-15 11:48:14 +0700

    feat: imports: vue-router, element-plus

---
+++ magner/lib/element-plus.ts
@@ -0,0 +1,39 @@
+import ruElLocale from 'element-plus/lib/locale/lang/ru';
+import enElLocale from 'element-plus/lib/locale/lang/en';
+import uzElLocale from 'element-plus/lib/locale/lang/uz-uz';
+
+import {
+  ElMessage,
+  ElNotification,
+  ElLoading,
+
+  ElTable,
+  ElTableColumn,
+  ElRow,
+
+  ElButton,
+  ElImage,
+  ElCascader,
+  ElSelect,
+  ElOption,
+} from 'element-plus';
+
+export {
+  ruElLocale,
+  enElLocale,
+  uzElLocale,
+
+  ElMessage,
+  ElNotification,
+  ElLoading,
+
+  ElTable,
+  ElTableColumn,
+  ElRow,
+
+  ElButton,
+  ElImage,
+  ElCascader,
+  ElSelect,
+  ElOption,
+};
