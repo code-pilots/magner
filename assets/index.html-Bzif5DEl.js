06795cb33c6ac6ea7bfe5dcf2558bdf986f9a554
Fedor531 <60313644+Fedor531@users.noreply.github.com>
2024-03-15 04:50:08 +0000

    Deploying to gh-pages from @ code-pilots/magner@22dd5737c4895926728c96de249477c99e3f0efd 🚀

---
+++ assets/index.html-Bzif5DEl.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-2bc0f532","path":"/configuration/","title":"Entry point","lang":"en","frontmatter":{},"headers":[],"git":{"updatedTime":1710478166000,"contributors":[{"name":"Fedor531","email":"60313644+Fedor531@users.noreply.github.com","commits":1}]},"filePathRelative":"configuration/readme.md"}');export{e as data};
