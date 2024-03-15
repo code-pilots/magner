0840d15d101acf5f01d297c59b9eb6556f157400
Fedor531 <60313644+Fedor531@users.noreply.github.com>
2024-03-15 04:52:50 +0000

    Deploying to gh-pages from @ code-pilots/magner@bba83293de90af829bc3492a4ae5c51393b61b63 🚀

---
+++ assets/manifest.html-Apb79gQ4.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-e991b7d0","path":"/configuration/manifest.html","title":"Manifest controller","lang":"en","frontmatter":{},"headers":[],"git":{"updatedTime":1710478328000,"contributors":[{"name":"Fedor531","email":"60313644+Fedor531@users.noreply.github.com","commits":1}]},"filePathRelative":"configuration/manifest.md"}');export{e as data};
